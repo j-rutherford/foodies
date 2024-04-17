7626eb7c6132c76a803d42d2003e9abad6878688
Jeremy Rutherford <jrutherford@centriq.com>
2024-04-17 17:11:43 -0500

    completed project

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,7 +3,13 @@ import { notFound } from "next/navigation";
 
 import { getMeal } from "@/lib/meals";
 import classes from "./page.module.css";
-
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+  return { title: meal.title, description: meal.summary };
+}
 export default function MealDetailsPage({ params }) {
   const meal = getMeal(params.mealSlug);
 
@@ -16,7 +22,11 @@ export default function MealDetailsPage({ params }) {
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image src={meal.image} alt={meal.title} fill />
+          <Image
+            src={`https://jeremyrutherford-nextjs-demo-users-image.s3.amazonaws.com/${meal.image}`}
+            alt={meal.title}
+            fill
+          />
         </div>
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
